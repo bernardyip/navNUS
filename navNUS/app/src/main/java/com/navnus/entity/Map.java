a2cfa9f402577de11f6c26a5f3e34dc59c98485d
Bernard Yip <bernard_yip40@hotmail.com>
2016-05-15 12:42:44 +0800

    Changed to DirectedGraphs, Started on A* Algorithm

--- navNUS/app/src/main/java/com/navnus/entity/Map.java
+++ navNUS/app/src/main/java/com/navnus/entity/Map.java
@@ -5,41 +5,23 @@ import android.content.Context;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Stack;
 
-import edu.princeton.cs.algs4.EdgeWeightedGraph;
+import edu.princeton.cs.algs4.DirectedEdge;
+import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 
 /**
  * Created by Legend on 15/5/2016.
  */
 public class Map {
     public static int numberOfVertices = 826;
-    public static EdgeWeightedGraph graph;
+    public static EdgeWeightedDigraph graph;
     public static Vertice[] vertices;
 
-    /*
-     * Estimates distance between two points in latitude and longitude
-     *
-     * lat1, lon1 Start point, lat2, lon2 End point
-     * @returns Distance in Meters
-     */
-    public static double estimateDistance(int vertice1Id, int vertice2Id) {
-        final int R = 6371; // Radius of the earth
-        double lat1 = vertices[vertice1Id].latitude;
-        double lon1 = vertices[vertice1Id].longitude;
-        double lat2 = vertices[vertice2Id].latitude;
-        double lon2 = vertices[vertice2Id].longitude;
-
-        Double latDistance = Math.toRadians(lat2 - lat1);
-        Double lonDistance = Math.toRadians(lon2 - lon1);
-        Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        double distance = R * c * 1000; // convert to meters
-
-        return Math.sqrt(distance);
-    }
-
     public static void initialize() {
-        graph = new EdgeWeightedGraph(numberOfVertices); //To modify later on
+        graph = new EdgeWeightedDigraph(numberOfVertices); //To modify later on
         vertices = new Vertice[numberOfVertices];
     }
 
@@ -68,7 +50,52 @@ public class Map {
         }
     }
 
-    public void loadEdges(String file) {
+    public static Iterable<DirectedEdge> getNeighbours(int verticeId) {
+        return graph.adj(verticeId);
+    }
 
+    public static void loadEdges(String file) {
+
+    }
+
+    /*
+     * Estimates distance between two points in latitude and longitude
+     *
+     * lat1, lon1 Start point, lat2, lon2 End point
+     * @returns Distance in Meters
+     */
+    public static double estimateDistance(Vertice vertice1, Vertice vertice2) {
+        final int R = 6371; // Radius of the earth
+        double lat1 = vertice1.latitude;
+        double lon1 = vertice1.longitude;
+        double lat2 = vertice2.latitude;
+        double lon2 = vertice2.longitude;
+
+        Double latDistance = Math.toRadians(lat2 - lat1);
+        Double lonDistance = Math.toRadians(lon2 - lon1);
+        Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        return Math.sqrt(distance);
+    }
+
+    public static Stack<DirectedEdge> getPath(int startVerticeId, int endVerticeId) {
+        LinkedList<DirectedEdge> openList = new LinkedList<DirectedEdge>();
+        HashSet<DirectedEdge> closedList = new HashSet<DirectedEdge>();
+        LinkedList<DirectedEdge> result = new LinkedList<DirectedEdge>();
+        int min, max, length;
+
+        //Iterate through openlist until none are left
+        while (!openList.isEmpty()) {
+            min = -1;
+            max = Integer.MAX_VALUE;
+
+            for(int i=0; i<openList.size(); i++) {
+                DirectedEdge edge = openList.removeFirst();
+            }
+
+        }
+        return null;
     }
 }
