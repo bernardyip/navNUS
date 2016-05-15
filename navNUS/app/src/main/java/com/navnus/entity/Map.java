a848adc0aacdd80a24ef1424662c54c54f366242
Bernard Yip <bernard_yip40@hotmail.com>
2016-05-15 12:09:42 +0800

    Added calculation for distance between 2 points

--- navNUS/app/src/main/java/com/navnus/entity/Map.java
+++ navNUS/app/src/main/java/com/navnus/entity/Map.java
@@ -16,6 +16,28 @@ public class Map {
     public static EdgeWeightedGraph graph;
     public static Vertice[] vertices;
 
+    /*
+     * Estimates distance between two points in latitude and longitude
+     *
+     * lat1, lon1 Start point, lat2, lon2 End point
+     * @returns Distance in Meters
+     */
+    public static double estimateDistance(int vertice1Id, int vertice2Id) {
+        final int R = 6371; // Radius of the earth
+        double lat1 = vertices[vertice1Id].latitude;
+        double lon1 = vertices[vertice1Id].longitude;
+        double lat2 = vertices[vertice2Id].latitude;
+        double lon2 = vertices[vertice2Id].longitude;
+
+        Double latDistance = Math.toRadians(lat2 - lat1);
+        Double lonDistance = Math.toRadians(lon2 - lon1);
+        Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        return Math.sqrt(distance);
+    }
+
     public static void initialize() {
         graph = new EdgeWeightedGraph(numberOfVertices); //To modify later on
         vertices = new Vertice[numberOfVertices];
